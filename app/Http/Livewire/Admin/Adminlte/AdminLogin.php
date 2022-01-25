6f385f23d1e9fef1b6b38d0cd80e29b4676ab977
Kazi Omar Faruk <kaziomar144@gmail.com>
2022-01-25 13:06:23 +0600

    product add to wishlist

---
+++ app/Http/Livewire/Admin/Adminlte/AdminLogin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Adminlte;
+
+use Livewire\Component;
+
+class AdminLogin extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.adminlte.admin-login');
+    }
+}
