74a84a9c7e8e6202c7c6ad937ec50e8f896f8bc4
Kazi Omar Faruk <kaziomar144@gmail.com>
2022-01-27 00:05:04 +0600

    Add save for later option in cart page

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -43,6 +43,32 @@ class CartComponent extends Component
         session()->flash('msg-type','success');
     }
 
+    //save for later function
+    public function switchToSaveForLater($rowId)
+    {
+        $item = Cart::instance('cart')->get($rowId);
+        Cart::instance('cart')->remove($rowId);
+        Cart::instance('saveForLater')->add($item->id,$item->name,1,$item->price)->associate('App\Models\Product');
+        $this->emitTo('cart-count-component','refreshComponent');
+        $this->dispatchBrowserEvent('toastr',['type' => 'Success','message'=>'Item has been saved for later']);
+
+    }
+
+    public function moveToCart($rowId)
+    {
+        $item = Cart::instance('saveForLater')->get($rowId);
+        Cart::instance('saveForLater')->remove($rowId);
+        Cart::instance('cart')->add($item->id,$item->name,1,$item->price)->associate('App\Models\Product');
+        $this->emitTo('cart-count-component','refreshComponent');
+        $this->dispatchBrowserEvent('toastr',['type' => 'Success','message'=>'Item has been moved to cart']);
+    }
+
+    public function deleteFromSaveForLater($rowId)
+    {
+        Cart::instance('saveForLater')->remove($rowId);
+        $this->dispatchBrowserEvent('toastr',['type' => 'Error','message'=>'Item has been removed from save for later']);
+    }
+
     public function render()
     {
         $sale = Sale::find(1);
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -43,7 +43,10 @@
 										<a class="btn btn-increase" href="#" wire:click.prevent="increaseQuantity('{{$item->rowId}}')"></a>
 										<a class="btn btn-reduce" href="#"wire:click.prevent="decreaseQuantity('{{$item->rowId}}')"></a>
 									</div>
+									{{-- SaveForLater Option --}}
+								<p class="text-center"><a href="#" wire:click.prevent="switchToSaveForLater('{{$item->rowId}}')">Save For Later</a></p>
 								</div>
+								
 								<div class="price-field sub-total"><p class="price">${{$item->subtotal}}</p></div>
 								<div class="delete">
 									<a href="#" class="btn btn-delete" title="Remove" wire:click.prevent="destroy('{{$item->rowId}}')">
@@ -79,8 +82,48 @@
 							<a class="btn btn-update" href="#">Update Shopping Cart</a>
 						</div>
 					</div>
+
+					{{-- Save for later content --}}
+					<div class="wrap-iten-in-cart">
+						<h3 class="title-box" style="border-bottom: 1px solid; padding-bottom:15px">{{Cart::instance('saveForLater')->count()}} item(s) Saved For Later</h3>
+						@if (Cart::instance('saveForLater')->count() > 0)
+							
+						
+						<h3 class="box-title">Products Name</h3>
+						<ul class="products-cart">
+							@foreach (Cart::instance('saveForLater')->content() as $item)							
+							<li class="pr-cart-item">
+								<div class="product-image">
+									<figure><img src="{{asset('assets')}}/images/products/{{$item->model->image}}" alt="{{$item->name}}"></figure>
+								</div>
+								<div class="product-name">
+									<a class="link-to-product" href="{{route('product.details',['slug'=>$item->model->slug])}}">{{$item->name}}</a>
+								</div>
+								@if ($item->model->sale_price && $sale->status == 1 && $sale->sale_date > Carbon\Carbon::now())
+								<div class="price-field produtc-price"><span class="price">${{$item->model->sale_price}}</span> <del><span class="price">${{$item->model->regular_price}}</span></del></div>
+								@else
+								<div class="price-field produtc-price"><p class="price">${{$item->model->regular_price}}</p></div>
+								@endif
+								
+								<div class="quantity">
+									{{-- SaveForLater Option --}}
+								<p class="text-center"><a href="#" wire:click.prevent="moveToCart('{{$item->rowId}}')">Move To Cart</a></p>
+								</div>
+								<div class="delete">
+									<a href="#" class="btn btn-delete" title="Remove" wire:click.prevent="deleteFromSaveForLater('{{$item->rowId}}')">
+										<span>Delete from save for later</span>
+										<i class="fa fa-times-circle" aria-hidden="true"></i>
+									</a>
+								</div>
+							</li>	
+							@endforeach											
+						</ul>
+						@else
+						<p>No item saved for later</p>
+						@endif
+					</div>
 	
-					<div class="wrap-show-advance-info-box style-1 box-in-site">
+					<div class="wrap-show-advance-info-box style-1 box-in-site" wire:ignore>
 						<h3 class="title-box">Most Viewed Products</h3>
 						<div class="wrap-products">
 							<div class="products slide-carousel owl-carousel style-nav-1 equal-container" data-items="5" data-loop="false" data-nav="true" data-dots="false" data-responsive='{"0":{"items":"1"},"480":{"items":"2"},"768":{"items":"3"},"992":{"items":"3"},"1200":{"items":"5"}}' >
@@ -88,7 +131,7 @@
 								<div class="product product-style-2 equal-elem ">
 									<div class="product-thumnail">
 										<a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-											<figure><img src="{{asset('assets')}}/images/products/digital_04.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+											<figure><img src="{{asset('assets')}}/images/products/digital_4.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
 										</a>
 										<div class="group-flash">
 											<span class="flash-item new-label">new</span>
@@ -143,7 +186,7 @@
 								<div class="product product-style-2 equal-elem ">
 									<div class="product-thumnail">
 										<a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-											<figure><img src="{{asset('assets')}}/images/products/digital_01.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+											<figure><img src="{{asset('assets')}}/images/products/digital_1.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
 										</a>
 										<div class="group-flash">
 											<span class="flash-item bestseller-label">Bestseller</span>
@@ -176,7 +219,7 @@
 								<div class="product product-style-2 equal-elem ">
 									<div class="product-thumnail">
 										<a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-											<figure><img src="{{asset('assets')}}/images/products/digital_03.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+											<figure><img src="{{asset('assets')}}/images/products/digital_3.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
 										</a>
 										<div class="group-flash">
 											<span class="flash-item sale-label">sale</span>
@@ -194,7 +237,7 @@
 								<div class="product product-style-2 equal-elem ">
 									<div class="product-thumnail">
 										<a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-											<figure><img src="{{asset('assets')}}/images/products/digital_04.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+											<figure><img src="{{asset('assets')}}/images/products/digital_4.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
 										</a>
 										<div class="group-flash">
 											<span class="flash-item new-label">new</span>
@@ -212,7 +255,7 @@
 								<div class="product product-style-2 equal-elem ">
 									<div class="product-thumnail">
 										<a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-											<figure><img src="{{asset('assets')}}/images/products/digital_05.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+											<figure><img src="{{asset('assets')}}/images/products/digital_5.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
 										</a>
 										<div class="group-flash">
 											<span class="flash-item bestseller-label">Bestseller</span>
