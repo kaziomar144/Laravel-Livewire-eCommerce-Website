c108dfe5ab08d5caa761c476b480226ec6ab6b7c
Kazi Omar Faruk <kaziomar144@gmail.com>
2022-02-02 23:08:45 +0600

    Implement settings foorer and contact page

--- app/Http/Livewire/ContactComponent.php
+++ app/Http/Livewire/ContactComponent.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Contact;
+use App\Models\Setting;
 use Livewire\Component;
 
 class ContactComponent extends Component
@@ -51,6 +52,7 @@ class ContactComponent extends Component
 
     public function render()
     {
-        return view('livewire.contact-component')->layout('layouts.base');
+        $setting = Setting::find(1);
+        return view('livewire.contact-component',compact('setting'))->layout('layouts.base');
     }
 }
