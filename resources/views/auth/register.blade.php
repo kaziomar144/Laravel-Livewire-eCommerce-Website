b6da47844ae32c46208ce54590d23dd77d7178a9
Kazi Omar Faruk <kaziomar144@gmail.com>
2022-02-03 14:06:04 +0600

    Change layout page~D:03/02/2022

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -59,7 +59,7 @@
     </x-jet-authentication-card>
 </x-guest-layout> --}}
 
-<x-guest-layout>
+<x-base-layout>
     <!--main area-->
 	<main id="main" class="main-site left-sidebar">
 
@@ -119,4 +119,4 @@
 
 	</main>
 	<!--main area-->
-</x-guest-layout>
+</x-base-layout>
