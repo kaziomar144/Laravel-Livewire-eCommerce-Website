d7fe1d9e714c756c0b64ebee7370fb6e64745819
Kazi Omar Faruk <kaziomar144@gmail.com>
2022-02-11 17:29:49 +0600

    Show subcategory on shop page

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -1,24 +1,25 @@
 <div>
-   <div class="container" style="padding:30px 0">
+    <div class="container" style="padding:30px 0">
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <div class="row">
-                            <div class="col-md-6">                                
+                            <div class="col-md-6">
                                 All Categories
                             </div>
                             <div class="col-md-6">
-                                <a href="{{route('admin.addcategory')}}" class="btn btn-success pull-right">Add New Category</a>
+                                <a href="{{route('admin.addcategory')}}" class="btn btn-success pull-right">Add New
+                                    Category</a>
                             </div>
                         </div>
                     </div>
                     <div class="panel-body">
                         @if (Session::has('msg'))
-							<div class="alert alert-{!! Session::get('msg-type') !!} ">
-								{{Session::get('msg')}}
-							</div>
-						@endif
+                        <div class="alert alert-{!! Session::get('msg-type') !!} ">
+                            {{Session::get('msg')}}
+                        </div>
+                        @endif
                         <table class="table table-striped">
                             <thead>
                                 <tr>
@@ -31,25 +32,34 @@
                             </thead>
                             <tbody>
                                 @foreach ($categories as $category)
-                                    <tr>
-                                        <td>{{$category->id}}</td>
-                                        <td>{{$category->name}}</td>
-                                        <td>{{$category->slug}}</td>
-                                        <td>
-                                            <ul style="list-style: none">
-                                                @foreach ($category->subcategories as $subcategory)
-                                                    <li><i class="fa fa-caret-right"></i> {{$subcategory->name}} 
-                                                        <a href="{{route('admin.editcategory',['category_slug'=>$category->slug,'scategory_slug'=>$subcategory->slug])}}"><i class="fa fa-edit"></i></a>
-                                                        <a href="#" onclick="confirm('Are you sure, You want to delete this subcategory?') || event.stopImmediatePropagation()" wire:click.prevent="deleteSubcategory({{$subcategory->id}})"><i class="fa fa-times text-danger"></i></a>
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-                                        </td>
-                                        <td>
-                                            <a href="{{route('admin.editcategory',['category_slug'=>$category->slug])}}" class="btn btn-success">Edit</a>
-                                            <a href="#" onclick="confirm('Are you sure, You want to delete this category?') || event.stopImmediatePropagation()" wire:click.prevent="deleteCategory({{$category->id}})" class="btn btn-danger">Delete</a>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td>{{$category->id}}</td>
+                                    <td>{{$category->name}}</td>
+                                    <td>{{$category->slug}}</td>
+                                    <td>
+                                        <ul style="list-style: none">
+                                            @foreach ($category->subcategories as $subcategory)
+                                            <li><i class="fa fa-caret-right"></i> {{$subcategory->name}}
+                                                <a
+                                                    href="{{route('admin.editcategory',['category_slug'=>$category->slug,'scategory_slug'=>$subcategory->slug])}}"><i
+                                                        class="fa fa-edit"></i></a>
+                                                <a href="#"
+                                                    onclick="confirm('Are you sure, You want to delete this subcategory?') || event.stopImmediatePropagation()"
+                                                    wire:click.prevent="deleteSubcategory({{$subcategory->id}})"><i
+                                                        class="fa fa-times text-danger"></i></a>
+                                            </li>
+                                            @endforeach
+                                        </ul>
+                                    </td>
+                                    <td>
+                                        <a href="{{route('admin.editcategory',['category_slug'=>$category->slug])}}"
+                                            class="btn btn-success">Edit</a>
+                                        <a href="#"
+                                            onclick="confirm('Are you sure, You want to delete this category?') || event.stopImmediatePropagation()"
+                                            wire:click.prevent="deleteCategory({{$category->id}})"
+                                            class="btn btn-danger">Delete</a>
+                                    </td>
+                                </tr>
                                 @endforeach
                             </tbody>
                         </table>
@@ -60,5 +70,5 @@
                 </div>
             </div>
         </div>
-   </div>
+    </div>
 </div>
